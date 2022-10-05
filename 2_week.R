70f08fcbb7e979db8b37d6f7bfffcfc059abca30
Mustafa Araçlı <mustafa1aracli@gmail.com>
2022-10-05 10:44:18 +0300

    Create 2_week.R

---
+++ 2_week.R
@@ -0,0 +1,14 @@
+# veri madenciliği 2.hafta ders kodlari
+
+# R kütüphanesine paket yükleme
+
+install.packages("class")
+library(class)
+
+# r in kendi içindeki ceri setlerine bakalım
+# data fonksiyonu kullannılır
+
+data()
+
+# r icindeki bir veri setini çağırma
+show(iris)
