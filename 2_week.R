d24351c7126c125f2ecea972d87bf4b99d749d6a
Mustafa Araçlı <mustafa1aracli@gmail.com>
2022-10-05 10:54:29 +0300

    Update 2_week.R

--- 2_week.R
+++ 2_week.R
@@ -12,3 +12,22 @@ data()
 
 # r icindeki bir veri setini çağırma
 show(iris)
+
+# str() fonksiyonu veri setinin ozetini verir
+# kac tane gözlem değeri
+# kaç tane degisken
+# degiskenlerin turu nedir
+str(iris)
+
+# attributes oznitelik komutu degiskenlerin isminini verir
+# sinifini verir
+# satir numaralarini verir
+attributes(iris)
+
+
+# head komutu veri setinde ilk 6 satiri verir
+head(iris)
+
+# ilk iki tarırı görmek istersem head yanina 2 eklemeliyim
+
+head(iris,2)
