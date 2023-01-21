d5d332ab286f870ebc96edc18bf435f3c437a399
Mustafa Araçlı <61080380+mustafaracli@users.noreply.github.com>
2023-01-21 15:57:11 +0300

    Update YouTube_commetsMining.R

--- YouTube_commetsMining.R
+++ YouTube_commetsMining.R
@@ -7,7 +7,7 @@ library(tidyverse)
 install.packages(c("tuber"))
 library(tuber)
 
-apikey <- "AIzaSyCW0n14lV1c6EoLFPlQ4z9N5E4gmaRVgws"
+apikey <- ""
 key <- Authenticate("youtube",
                     apikey=apikey)
 
@@ -19,6 +19,6 @@ videos z-
 install.packages("tuber")
 library(tuber)
 
-yt_oauth("544163486916-negrcuc8po3pppl51vr1b073qsjo2vus.apps.googleusercontent.com", "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")
+yt_oauth("", "")
 
 get_stats(video_id = "yoyb0LtHbfc")
