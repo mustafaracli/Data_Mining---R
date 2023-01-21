dcc9c7601dd4f6671c7499a46be1ed1459ab0725
Mustafa Araçlı <61080380+mustafaracli@users.noreply.github.com>
2023-01-21 15:57:46 +0300

    Update r_youtube.R

--- r_youtube.R
+++ r_youtube.R
@@ -1,7 +1,7 @@
 install.packages("tuber")
 library(tuber)
 
-yt_oauth("544163486916-negrcuc8po3pppl51vr1b073qsjo2vus.apps.googleusercontent.com", "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")
+yt_oauth("", "")
 
 get_all_comments(video_id = "wFo-_tk6xmU")
 
